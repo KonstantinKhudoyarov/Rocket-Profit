88826f85a3773c953f2f2948b1f1b447b73abdc6
Konstantin Khudoyarov <kostya040390@gmail.com>
2019-04-14 14:41:03 +0300

    add adaptive menu

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,12 @@
-const lang = document.querySelector('.lang');
-const langList = document.querySelector('.lang__list');
+const lang = document.querySelector(".lang");
+const langList = document.querySelector(".lang__list");
+const mobMenu = document.querySelector(".menu__mob");
+const menuListMob = document.querySelector(".menu__list_mob");
 
-lang.addEventListener('click', function () {
-    this.classList.toggle('lang_active');
+lang.addEventListener("click", function() {
+  this.classList.toggle("lang_active");
+});
+
+mobMenu.addEventListener("click", function() {
+  this.classList.toggle("menu__mob_active");
 });
