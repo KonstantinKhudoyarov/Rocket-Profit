98ac2640d551a69405816a7a1829262ea0c0ca1b
Xudko04031990 <kostya040390@gmail.com>
2019-04-17 11:20:18 +0300

    add adaptive menu

--- source/js/app.js
+++ source/js/app.js
@@ -1,12 +1,36 @@
-const lang = document.querySelector(".lang");
-const langList = document.querySelector(".lang__list");
-const mobMenu = document.querySelector(".menu__mob");
-const menuListMob = document.querySelector(".menu__list_mob");
+(function () {
+  const menu = document.querySelector('.menu');
+  const lang = document.querySelector(".lang");
+  const mobMenu = document.querySelector(".menu__mob");
 
-lang.addEventListener("click", function() {
-  this.classList.toggle("lang_active");
-});
+  lang.addEventListener("click", function () {
+    this.classList.toggle("lang_open");
+  });
 
-mobMenu.addEventListener("click", function() {
-  this.classList.toggle("menu__mob_active");
-});
+  mobMenu.addEventListener("click", function () {
+    menu.classList.toggle("menu_open");
+  });
+
+
+})();
+
+(function () {
+
+  const mobileMenuBtnOpen = document.querySelector('.menu__mob_small');
+  const mobileMenuBtnClose = document.querySelector('.mobile-menu__mob');
+  const mobileMenuOverlay = document.querySelector('.mobile-menu');
+  const langMobile = document.querySelector('.mobile-menu__lang');
+
+  mobileMenuBtnOpen.addEventListener('click', function () {
+    mobileMenuOverlay.classList.add('mobile-menu_open');
+  });
+
+  mobileMenuBtnClose.addEventListener('click', function () {
+    mobileMenuOverlay.classList.remove('mobile-menu_open');
+  });
+
+  langMobile.addEventListener('click', function () {
+    this.classList.toggle('mobile-menu__lang_open');
+  });
+
+})();
